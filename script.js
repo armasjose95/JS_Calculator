379c95e924886f3983aa2b6f1fb46325ac0b85da
Jose Armas <armasjose95@gmail.com>
2022-12-01 23:27:25 -0600

    num functions

--- script.js
+++ script.js
@@ -15,14 +15,27 @@ class Calculator { //allows us to set these text element inside of caluclator cl
     delete() {
 
     }
+
     //what's going to happen every time a user clicks on a # to add to the screen. Pass the # the user selected. 
+    //convert everything to a sting because JS will try to add these as actual numbers. ex 1+1=11 not 2. 
+    //#s need to be appended not added
     appendNumber(number) { //instead of appending the #, we can just change the current operand to equal that #
-        this.currentOperand = number
+        //Only want one period so checking to see if we already have one
+        if (number === '.' && this.currentOperand.includes('.')) return
+        if(this.currentOperand === '' )
+        {
+            this.currentOperand = number
+        }
+        else{
+            this.currentOperand = this.currentOperand.toString() + number.toString()
+        }
     }
 
     //what's going to happen every time a user clicks on an operation. ex. +,- Pass the operation the user selected. 
     chooseOperation(operation) {
-
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     //Takes values inside of calculator & compute a single value for what we need to display on the calculator.
@@ -33,6 +46,7 @@ class Calculator { //allows us to set these text element inside of caluclator cl
     //update the values inside of our output
     updateDisplay() { //allows us to set the text of number in white
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -53,5 +67,12 @@ numberButtons.forEach(button => {
     button.addEventListener('click', () => { //event listener looking out for a click
         calculator.appendNumber(button.innerText) //want to add(append) the # of whatever is inside that button
         calculator.updateDisplay() //our display valueus will constantly be updated every time we click a button 
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => { //event listener looking out for a click
+        calculator.chooseOperation(button.innerText) //want to choose the operation
+        calculator.updateDisplay() //our display valueus will constantly be updated every time we click an operation 
     })
 })
