75b4e89e80d8cbeca06ad5b769a32659e6028cc7
Jose Armas <armasjose95@gmail.com>
2022-12-04 18:27:17 -0600

    final

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ class Calculator { //allows us to set these text element inside of caluclator cl
     }
 
     delete() {
-
+        /* convert curroperand after setting to itself & want to get very last value and chop it off, hence -1
+        that's what slice method is for, goes from index 0 all the way to one from the end
+        takes all characters from currOper sting & all diff #s from very 1st # to 2ndtolast and say that in new currOperand #*/
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     //what's going to happen every time a user clicks on a # to add to the screen. Pass the # the user selected. 
@@ -28,7 +31,7 @@ class Calculator { //allows us to set these text element inside of caluclator cl
 
     //what's going to happen every time a user clicks on an operation. ex. +,- Pass the operation the user selected. 
     chooseOperation(operation) {
-        //checkin to make sure operations dont work even if there's no crrnt value to operate on. cant execute further into code
+        //checkin to make sure operations dont work if there's no crrnt value to operate on. cant execute further into code
         if (this.currentOperand === '') return 
         //if i already have something computed in prevopperand & something in curroperand, I want to do computation
         if (this.previousOperand !== '') { 
@@ -73,10 +76,39 @@ class Calculator { //allows us to set these text element inside of caluclator cl
         this.previousOperand = ''
     }
 
+    //add commas where needed in larger numbers
+    getDisplayNumber(number) { //also calling the function in the updateDisplay function
+        //need string cus we want to be able to split string on the decimail character inside of it
+        const stringNumber = number.toString() 
+        //split on actual decimal. take our string & turn it into an array. 1st # pt of array is b4 period & 2nd # is after period
+        //get the 1st part[0] cus we want the integer values b4 that
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        //dont need to parse cus we dont need it to be a # so we want 2nd part of that array [1]
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay //want integerDisplay seperatley 
+        if (isNaN(integerDigits)) { //happens anytime you input nothing on screen or inputs just a decimal place
+            integerDisplay = '' //if it happens, want integerDisplay to be equal to an empty string
+        } else { // if they did enter an integer value
+            integerDisplay = integerDigits.toLocaleString('en', {
+                // never want a decimal place after it gets converted to a string with a bunch of commas
+                maximumFractionDigits: 0 }) 
+        }
+        if (decimalDigits != null) { // if there is a decimal and not equal to null
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay // if there's no decimal
+        }
+    }
+
     //update the values inside of our output
     updateDisplay() { //allows us to set the text of number in white
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) { // if we have an operation and operation does not equal null. We want to display prev opp txt
+            //make a concatenation of both prevOpper & our operation. string has the operation appended 2 the end of it
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else { //clear out previous opperand after AC button is pressed
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
@@ -109,5 +141,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => { //passes in the button
     calculator.compute() //calls compute bttn in my calculator to get the computed value
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => { //passes in the button
+    calculator.clear() //calls compute bttn in my calculator to get the computed value
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => { //passes in the button
+    calculator.delete() //calls compute bttn in my calculator to get the computed value
     calculator.updateDisplay()
 })
